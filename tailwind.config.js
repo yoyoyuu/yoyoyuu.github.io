1643a9c2d63270df0a436e4032a73c8e51e7485d
yoyoyuu <isisvanesa250@gmail.com>
2025-12-11 22:01:28 -0800

    Changed whole style to use TailwindCSS framework + Edited 'about' page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        'primary': '#972469',
-        'secondary': '#72124c',
-        'light-pink': '#ffd4ec',
-        'dark-pink': '#9c276d',
-        'highlight': '#ffb3d9',
-        'hover-pink': '#d13a9c',
+        'brand-pink': '#972469',
+        'brand-light-pink': '#ffd4ec',
+        'brand-dark-pink': '#72124c',
+        'brand-hot-pink': '#9c276d',
       },
       fontFamily: {
-        'rock-bold': ['Rock Bold', 'serif'],
+        'rock-bold': ['Rock Bold', 'sans-serif'],
         'trebuchet': ['Trebuchet MS', 'sans-serif'],
       },
-      backgroundImage: {
-        'body-pattern': "url('/img_index/fondo-.gif')",
-        'gradient-pink': 'linear-gradient(0deg, #FFFFFF 0%, #ffd4ec 100%)',
-      },
-      spacing: {
-        '128': '32rem',
-      },
       boxShadow: {
-        'neon-pink': '2px 0 0 #972469, 0 2px 0 #972469, -2px 0 0 #972469, 0 -2px 0 #972469',
-        'card': '0 0 0 10px #FFFFFF',
+        'neon': '2px 0 0 #972469, 0 2px 0 #972469, -2px 0 0 #972469, 0 -2px 0 #972469',
       },
+      backgroundImage: {
+        'gradient-pink': 'linear-gradient(0deg, #FFFFFF 0%, #ffd4ec 100%)',
+      }
     },
   },
   plugins: [],
