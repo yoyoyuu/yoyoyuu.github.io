66c25bf446e0d6fa9b35848dc1db26290583684a
yoyoyuu <isisvanesa250@gmail.com>
2025-12-12 20:36:41 -0800

    Refactor layout and styles across multiple components, enhance navigation, and add new pages for commissions and projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        'brand-pink': '#972469',
-        'brand-light-pink': '#ffd4ec',
-        'brand-dark-pink': '#72124c',
-        'brand-hot-pink': '#9c276d',
+        'brand': {
+          'pink': '#972469',
+          'light-pink': '#ffd4ec',
+          'dark-pink': '#72124c',
+          'hot-pink': '#9c276d',
+        }
       },
       fontFamily: {
         'rock-bold': ['Rock Bold', 'sans-serif'],
         'trebuchet': ['Trebuchet MS', 'sans-serif'],
       },
-      boxShadow: {
-        'neon': '2px 0 0 #972469, 0 2px 0 #972469, -2px 0 0 #972469, 0 -2px 0 #972469',
-      },
-      backgroundImage: {
-        'gradient-pink': 'linear-gradient(0deg, #FFFFFF 0%, #ffd4ec 100%)',
+      spacing: {
+        'page': '1rem', // 16px - padding general de página
+        'content': '1.25rem', // 20px - padding interno de content-box
+        'box': '1.5rem', // 24px - margin entre cajas
       }
     },
   },
